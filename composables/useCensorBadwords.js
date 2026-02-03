823099efef164eac4bea90d38fa48fad67d4bbb2
selimdoyranli <selimdoyranli@gmail.com>
2026-02-03 14:43:58 +0300

    refactor: update censor badwords logic

--- composables/useCensorBadwords.js
+++ composables/useCensorBadwords.js
@@ -1,11 +1,8 @@
-import { useContext } from '@nuxtjs/composition-api'
 import { badWords as badWordsTr } from '@/locales/badwords/badwords-tr'
 import { badWords as badWordsEn } from '@/locales/badwords/badwords-en'
 
 export default () => {
-  const { i18n } = useContext()
-
-  const badWords = i18n.locale === i18n.defaultLocale ? badWordsTr : badWordsEn
+  const badWords = [...badWordsTr, ...badWordsEn]
 
   const censorBadwords = text => {
     if (typeof text !== 'string') return ''
