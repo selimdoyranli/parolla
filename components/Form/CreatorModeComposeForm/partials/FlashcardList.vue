d2ae9aebd21234dd24551ef73e665d073c78d352
selimdoyranli <selimdoyranli@gmail.com>
2026-04-08 19:14:08 +0300

    feat: add FlashcardList component

---
+++ components/Form/CreatorModeComposeForm/partials/FlashcardList.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+.flashcard-list
+  span.creator-mode-compose-form__fieldsTitle {{ $t('form.creatorModeCompose.flashcards.set') }}
+  .creator-mode-compose-form__fields
+    .compose-qa-list
+      // Flashcard List
+      template(v-if="flashcardList && flashcardList.length > 0")
+        .flashcard-list-item(v-for="(item, index) in flashcardList" :key="item.id")
+          FlashcardItem(
+            :item="item"
+            :index="index"
+            :is-last="index === flashcardList.length - 1"
+            @remove="$emit('remove', index)"
+            @move-up="$emit('move-up', index)"
+            @move-down="$emit('move-down', index)"
+          )
+
+      // Empty List
+      template(v-else)
+        Empty.flashcard-list-empty
+          template(#image)
+            Icon(name="tabler:cards" size="64")
+          p.flashcard-list-empty__description {{ $t('form.creatorModeCompose.flashcards.empty.description') }}
+          Button.compose-qa-list__addQaButton(type="info" icon="plus" native-type="button" round @click="$emit('add-item')")
+            | {{ $t('form.creatorModeCompose.flashcards.empty.action') }}
+
+      // Add flashcard button
+      Button.compose-qa-list__addQaButton(
+        v-if="flashcardList && flashcardList.length > 0"
+        type="info"
+        icon="plus"
+        plain
+        native-type="button"
+        round
+        :loading="isBusy"
+        :disabled="isBusy"
+        @click="$emit('add-item')"
+      )
+        | {{ $t('form.creatorModeCompose.flashcards.addMore.action') }}
+
+    p.creator-mode-compose-form__termsDescription
+      | {{ $t('form.creatorModeCompose.termsDescription') }}
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import { Empty, Button } from 'vant'
+import FlashcardItem from './FlashcardItem.vue'
+
+export default defineComponent({
+  name: 'FlashcardList',
+  components: {
+    Empty,
+    Button,
+    FlashcardItem
+  },
+  props: {
+    flashcardList: {
+      type: Array,
+      default: () => []
+    },
+    isBusy: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
+
+<style lang="scss" src="./FlashcardList.scss"></style>
