6fed3faa1f3c1f5a8e506a3940ea8aff35becf3e
selimdoyranli <selimdoyranli@gmail.com>
2026-05-03 17:01:38 +0300

    chore: update ads

--- components/Ad/AppAd/AppAd.component.vue
+++ components/Ad/AppAd/AppAd.component.vue
@@ -1,23 +1,15 @@
 <template lang="pug">
-.rota-block
-  a.rota-block(
-    href="https://rota.tips?utm_source=parolla.app"
-    title="Şehir Rehberi & Gezi Planlayıcı | Mekan Keşfet"
-    rel="noopener noreferrer"
-    target="_blank"
+.app-add
+  script(src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5310998423283624" async crossorigin="anonymous")
+  ins.adsbygoogle(
+    style="display: block"
+    data-ad-client="ca-pub-5310998423283624"
+    :data-ad-slot="dataAdSlot"
+    data-ad-format="auto"
+    data-full-width-responsive="true"
   )
-    img.rota-block__image(src="/block/rota.jpeg" alt="Şehir Rehberi & Gezi Planlayıcı | Mekan Keşfet" decoding="async")
-  //
-    script(src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5310998423283624" async crossorigin="anonymous")
-    ins.adsbygoogle(
-      style="display: block"
-      data-ad-client="ca-pub-5310998423283624"
-      :data-ad-slot="dataAdSlot"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    )
-    script.
-      (adsbygoogle = window.adsbygoogle || []).push({});
+  script.
+    (adsbygoogle = window.adsbygoogle || []).push({});
 </template>
 
 <script>
