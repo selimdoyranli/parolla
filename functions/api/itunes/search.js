7085be32c43f95d3a59e55f5365813af14641ed4
selimdoyranli <selimdoyranli@gmail.com>
2026-06-17 17:57:45 +0300

    chore: update ua for itunes worker

--- functions/api/itunes/search.js
+++ functions/api/itunes/search.js
@@ -4,7 +4,8 @@ export async function onRequest(context) {
 
   const response = await fetch(itunesUrl, {
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36 Edg/149.0.0.0'
     }
   })
 
